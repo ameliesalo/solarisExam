92227d3f8b9c00f9d2927f9cad0f7f250c3e659e
ameliesalo <145265503+ameliesalo@users.noreply.github.com>
2024-12-02 15:01:26 +0100

    two

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const jupiter = document.querySelector('.jupiter')
 const saturnus = document.querySelector('.saturnus')
 const uranus = document.querySelector('.uranus')
 const neptunus = document.querySelector('.neptunus')
+const planet = document.querySelector('.planet')
+
+let planetName = ('.name')
+let latin = ('.latin')
+let info = ('.info')
 
 const getKey = (apiType, endpoint) => {
     const url = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com' + endpoint
@@ -36,5 +41,17 @@ const getPlanets = async (apiType, endpoint, key) => {
     const bodiesDataJson = await bodiesData.json()
 
     console.log(bodiesDataJson)
+
+    sun.addEventListener('click', () => {
+        window.location.replace('http://127.0.0.1:5500/solarisExam/solarisExam/planet.html')
+        planetName.innerText = bodiesDataJson[0].name
+        latin.innerText = bodiesDataJson[0].latinName
+        info.innerText = bodiesDataJson[0].desc
+    })
 }
+
 getKey('POST', '/keys')
+
+planet.addEventListener('click', () => {
+    window.location.replace('http://127.0.0.1:5500/solarisExam/solarisExam/index.html')
+})
