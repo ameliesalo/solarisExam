70f82a1ecfb39f81d5d5ed51b95be793a565b973
ameliesalo <145265503+ameliesalo@users.noreply.github.com>
2024-12-02 19:31:39 +0100

    three

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ const uranus = document.querySelector('.uranus')
 const neptunus = document.querySelector('.neptunus')
 const planet = document.querySelector('.planet')
 
-let planetName = ('.name')
-let latin = ('.latin')
-let info = ('.info')
+let planetName = document.querySelector('.name')
+let latin = document.querySelector('.latin')
+let info = document.querySelector('.info')
+
+const bodiesDataJson = []
 
 const getKey = (apiType, endpoint) => {
     const url = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com' + endpoint
@@ -41,14 +43,21 @@ const getPlanets = async (apiType, endpoint, key) => {
     const bodiesDataJson = await bodiesData.json()
 
     console.log(bodiesDataJson)
+    console.log(JSON.stringify(bodiesDataJson.bodies[0].name))
+    console.log(bodiesDataJson.bodies[0].name)
 
     sun.addEventListener('click', () => {
+        console.log(bodiesDataJson.bodies[0].name)
         window.location.replace('http://127.0.0.1:5500/solarisExam/solarisExam/planet.html')
-        planetName.innerText = bodiesDataJson[0].name
-        latin.innerText = bodiesDataJson[0].latinName
-        info.innerText = bodiesDataJson[0].desc
+        planetName.innerText = bodiesDataJson.bodies[0].name
+        latin.innerText = bodiesDataJson.bodies[0].latinName
+        info.innerText = bodiesDataJson.bodies[0].desc
     })
+
+    
+    
 }
+
 
 getKey('POST', '/keys')
 
