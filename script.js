7ca763d57d76f930c6f994040766bc4fb2982a97
ameliesalo <145265503+ameliesalo@users.noreply.github.com>
2024-12-04 08:34:58 +0100

    five

--- script.js
+++ script.js
@@ -43,10 +43,16 @@ const getPlanets = async (apiType, endpoint, key) => {
     const bodiesDataJson = await bodiesData.json()
 
     console.log(bodiesDataJson)
-    console.log(JSON.stringify(bodiesDataJson.bodies[0].name))
-    console.log(bodiesDataJson.bodies[0].name)
+    
+    
 
-    sun.addEventListener('click', sun)
+    sun.addEventListener('click', () => {
+        console.log(bodiesDataJson.bodies[0].name)
+        window.location.replace('http://127.0.0.1:5500/solarisExam/solarisExam/planet.html')
+        planetName.innerText = bodiesDataJson.bodies[0].name
+        latin.innerText = bodiesDataJson.bodies[0].latinName
+        info.innerText = bodiesDataJson.bodies[0].desc
+    })
 
     
     
@@ -55,13 +61,9 @@ const getPlanets = async (apiType, endpoint, key) => {
 getKey('POST', '/keys')
 
 function solis(){
-    console.log(bodiesDataJson.bodies[0].name)
-    window.location.replace('http://solarisExam/solarisExam/planet.html')
-    planetName.innerText = bodiesDataJson.bodies[0].name
-    latin.innerText = bodiesDataJson.bodies[0].latinName
-    info.innerText = bodiesDataJson.bodies[0].desc
+    
 }
 
 planet.addEventListener('click', () => {
-    window.location.replace('http://solarisExam/solarisExam/index.html')
+    window.location.replace('http://127.0.0.1:5500/solarisExam/solarisExam/index.html')
 })
